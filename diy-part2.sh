2e4565bfb0fd5abc2c07b0d1195817f653c68816
vulcanford <fordvulcan@gmail.com>
2025-11-07 21:38:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,7 +23,6 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 sed -i 's/LEDE/iStoreOS/g' package/base-files/files/bin/config_generate
 sed -i 's/LEDE/iStoreOS/g' package/base-files/files/etc/init.d/system
 sed -i 's/LEDE/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's/LEDE/iStoreOS/g' package/base-files/luci2/bin/config_generate
 sed -i 's/LEDE/iStoreOS/g' package/lean/default-settings/files/zzz-default-settings
 
 curl -o package/base-files/files/etc/banner https://raw.githubusercontent.com/istoreos/istoreos/refs/heads/istoreos-22.03/package/base-files/files/etc/banner
